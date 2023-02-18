ec8784bd935d7b988f1d5197df7381bf6ba31a9d
sujey22 <da0089032022@unab.edu.sv>
2023-02-18 07:55:27 -0600

    creando la parte de la lógica

--- Negocio/ClsPalabras.cs
+++ Negocio/ClsPalabras.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MerlinSujeyDuarteArgueta18022023.Entidades;
 
 namespace MerlinSujeyDuarteArgueta18022023.Negocio
 {
     public class ClsPalabras
     {
-        
+        public int Contador(Palabras palabras){
+            return palabras.palabras.Length;
+        }
     }
 }
