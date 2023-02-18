a597a2f2e5101029f2943df66ca31d8fb613e0e3
sujey22 <da0089032022@unab.edu.sv>
2023-02-18 08:07:36 -0600

    Creando la vista

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using MerlinSujeyDuarteArgueta18022023.Entidades;
+using MerlinSujeyDuarteArgueta18022023.Negocio;
+
+Palabras palabras = new Palabras();
+ClsPalabras clsPalabras = new ClsPalabras();
+
+Console.WriteLine("Digite su palabra");
+palabras.palabras = Console.ReadLine();
+
+Console.WriteLine("El numero de letras que hay en la palabra es: "+ clsPalabras.Contador(palabras));
+
+
+
